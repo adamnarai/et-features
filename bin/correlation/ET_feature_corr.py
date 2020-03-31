3cea7807e09e48eb53743c696f97db13066551f1
Ádám Nárai <narai.adam@gmail.com>
2020-03-31 20:19:08 +0200

    make correlations interactive

--- bin/correlation/ET_feature_corr.py
+++ bin/correlation/ET_feature_corr.py
@@ -12,6 +12,7 @@ import pandas as pd
 import pingouin as pg
 import numpy as np
 import seaborn as sns
+from functools import partial
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from na_py_tools.defaults import RESULTS_DIR, SETTINGS_DIR
@@ -35,54 +36,79 @@ os.makedirs(results_dir, exist_ok=True)
 
 
 # %% Generate pdf figures
-for study in list(p['studies'].keys()):
+for study in ['dys', 'dys_contr_2']:#, 'dys_contr_2', 'letter_spacing']:#list(p['studies'].keys()):
     print('Running study: {}'.format(study))
-    with PdfPages(results_dir + '/ET_feature_corr_' + study + '.pdf') as pdf:      
-        # Correlation heatmaps
-        for gp in list(p['studies'][study]['groups'].values()):
-            for cond in p['studies'][study]['experiments']['et']['conditions']:
-                print('Running corr heatmap group: {}, cond: {}'.format(gp, cond))
-                corr_data = data[(data['study'] == study) & (data['condition'] == cond) 
-                                 & (data['group'] == gp)].loc[:, meas_list]
+    # with PdfPages(results_dir + '/ET_feature_corr_' + study + '.pdf') as pdf:      
+    # Correlation heatmaps
+    for gp in ['control']:#list(p['studies'][study]['groups'].values()):
+        for cond in ['SP2']:#p['studies'][study]['experiments']['et']['conditions']:
+            print('Running corr heatmap group: {}, cond: {}'.format(gp, cond))
+            corr_data = data[(data['study'] == study) & (data['condition'] == cond) 
+                             & (data['group'] == gp)].loc[:, meas_list]
+            labels = data[(data['study'] == study) & (data['condition'] == cond) 
+                             & (data['group'] == gp)].subj_id
+            
+            # Spearman correlation (p values in upper triangle)
+            r = corr_data.rcorr(method='spearman', stars=False, decimals=4)
+            r = r.replace('-', 1).apply(pd.to_numeric)
+            
+            # Triangle mask
+            mask = np.zeros_like(r, dtype=np.bool)
+            mask[np.triu_indices_from(mask)] = True
+            
+            # All r value
+            fig = plt.figure(figsize=(18, 14))
+            sns.heatmap(r,
+                        vmin=-1,
+                        vmax=1,
+                        cmap='RdBu_r',
+                        annot=True,
+                        fmt=".2f",
+                        annot_kws={"fontsize":8},
+                        mask=mask)
+            plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) ',
+                      fontsize=18, fontweight='bold')
+            # pdf.savefig()
+            # plt.close()
+            
+            # Significant r values
+            mask[(r.T >= .05)] = True
+            
+            fig = plt.figure(figsize=(18, 14))
+            def onclick(event, corr_data, labels):  
+                x = int(event.xdata)
+                y = int(event.ydata)
                 
-                # Spearman correlation (p values in upper triangle)
-                r = corr_data.rcorr(method='spearman', stars=False, decimals=4)
-                r = r.replace('-', 1).apply(pd.to_numeric)
+                # Spearman correlation
+                regr_stats = pg.corr(corr_data.iloc[:,x], corr_data.iloc[:,y], method='spearman')
+                r, pval, outliers = pg.correlation.skipped(corr_data.iloc[:,x], corr_data.iloc[:,y], method='spearman')
+            
+                # Regrplot
+                plt.figure(figsize=(7, 5.5))
+                ax = sns.regplot(x=corr_data.columns[x], y=corr_data.columns[y], data=corr_data)
                 
-                # Triangle mask
-                mask = np.zeros_like(r, dtype=np.bool)
-                mask[np.triu_indices_from(mask)] = True
-                
-                # All r value
-                fig = plt.figure(figsize=(18, 14))
-                sns.heatmap(r,
-                            vmin=-1,
-                            vmax=1,
-                            cmap='RdBu_r',
-                            annot=True,
-                            fmt=".2f",
-                            annot_kws={"fontsize":8},
-                            mask=mask)
-                plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) ',
-                          fontsize=18, fontweight='bold')
-                pdf.savefig()
-                plt.close()
-                
-                # Significant r values
-                mask[(r.T >= .05)] = True
-                fig = plt.figure(figsize=(18, 14))
-                sns.heatmap(r,
-                            vmin=-1,
-                            vmax=1,
-                            cmap='RdBu_r',
-                            annot=True,
-                            fmt=".2f",
-                            annot_kws={"fontsize":8},
-                            mask=mask)
-                plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) significant (p<0.05)',
-                          fontsize=18, fontweight='bold')
-                pdf.savefig()
-                plt.close()
+                for i, txt in enumerate(labels):
+                    ax.annotate(txt, (corr_data.iloc[i,x], corr_data.iloc[i,y]))
+                plt.title('study: {} | group: {} | condition: {}\n'.format(study, gp, cond)
+                          + regr_stats.to_string(col_space=10) + '\n' 
+                          + 'skipped: r = {:.2f}  p = {:.4f}  outliers: {}'.format(r, pval, ', '.join(labels[outliers])))
+                plt.subplots_adjust(top=0.85)
+            
+            curr_func = partial(onclick, corr_data=corr_data, labels=labels)
+            fig.canvas.mpl_connect('button_press_event', curr_func)
+            sns.heatmap(r,
+                        vmin=-1,
+                        vmax=1,
+                        cmap='RdBu_r',
+                        annot=True,
+                        fmt=".2f",
+                        annot_kws={"fontsize":8},
+                        mask=mask)
+            plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) significant (p<0.05)',
+                      fontsize=18, fontweight='bold')
+            plt.subplots_adjust(left=0.2, right=0.9, top=0.9, bottom=0.2)
+                # pdf.savefig()
+                # plt.close()
         
         # Regrplots
         if run_regrplots:
--- bin/correlation/all_corr.py
+++ bin/correlation/all_corr.py
@@ -12,6 +12,7 @@ import pandas as pd
 import pingouin as pg
 import numpy as np
 import seaborn as sns
+from functools import partial
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from na_py_tools.defaults import RESULTS_DIR, SETTINGS_DIR
@@ -49,50 +50,80 @@ os.makedirs(results_dir, exist_ok=True)
 # %% Generate pdf figures
 for study in ['dys', 'dys_contr_2']:
     print('Running study: {}'.format(study))
-    with PdfPages(results_dir + '/all_corr_' + study + '.pdf') as pdf:      
-        # Correlation heatmaps
-        for gp in list(p['studies'][study]['groups'].values()):
-            for cond in p['studies'][study]['experiments']['et']['conditions']:
-                print('Running corr heatmap group: {}, cond: {}'.format(gp, cond))
-                corr_data = data[(data['study'] == study) & (data['condition'] == cond) 
-                                 & (data['group'] == gp)].loc[:, meas_list]
-                corr_data = corr_data.dropna(axis='columns', how='all')
+    # with PdfPages(results_dir + '/all_corr_' + study + '.pdf') as pdf:      
+    # Correlation heatmaps
+    for gp in ['control']:#list(p['studies'][study]['groups'].values()):
+        for cond in ['SP2']:#p['studies'][study]['experiments']['et']['conditions']:
+            print('Running corr heatmap group: {}, cond: {}'.format(gp, cond))
+            corr_data = data[(data['study'] == study) & (data['condition'] == cond) 
+                             & (data['group'] == gp)].loc[:, meas_list]
+            corr_data = corr_data.dropna(axis='columns', how='all')
+            labels = data[(data['study'] == study) & (data['condition'] == cond) 
+                             & (data['group'] == gp)].subj_id
+            
+            # Spearman correlation (p values in upper triangle)
+            r = corr_data.rcorr(method='spearman', stars=False, decimals=4)
+            r = r.replace('-', 1).apply(pd.to_numeric, errors='coerce')
+            
+            # Triangle mask
+            mask = np.zeros_like(r, dtype=np.bool)
+            mask[np.triu_indices_from(mask)] = True
+            
+             # All r value
+            fig = plt.figure(figsize=(18, 14))
+            sns.heatmap(r,
+                        vmin=-1,
+                        vmax=1,
+                        cmap='RdBu_r',
+                        annot=True,
+                        fmt=".2f",
+                        annot_kws={"fontsize":6},
+                        mask=mask)
+            plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) ',
+                      fontsize=18, fontweight='bold')
+            # pdf.savefig()
+            # plt.close()
+            
+            # Significant r values
+            mask[(r.T >= .05)] = True
+            
+            fig = plt.figure(figsize=(18, 14))
+            def onclick(event, corr_data, labels, study, gp, cond):  
+                x = int(event.xdata)
+                y = int(event.ydata)
+                                
+                # Remove outliers
+                corr_data_sub = corr_data.dropna(subset=list(corr_data.columns[[x, y]]))
+                labels_sub = labels[corr_data.iloc[:,x].notna() & corr_data.iloc[:,y].notna()]
                 
-                # Spearman correlation (p values in upper triangle)
-                r = corr_data.rcorr(method='spearman', stars=False, decimals=4)
-                r = r.replace('-', 1).apply(pd.to_numeric)
+                # Spearman correlation
+                regr_stats = pg.corr(corr_data_sub.iloc[:,x], corr_data_sub.iloc[:,y], method='spearman')
+                r, pval, outliers = pg.correlation.skipped(corr_data_sub.iloc[:,x], 
+                                                           corr_data_sub.iloc[:,y], method='spearman')
+            
+                # Regrplot
+                plt.figure(figsize=(7, 5.5))
+                ax = sns.regplot(x=corr_data_sub.columns[x], y=corr_data_sub.columns[y], data=corr_data_sub)
                 
-                # Triangle mask
-                mask = np.zeros_like(r, dtype=np.bool)
-                mask[np.triu_indices_from(mask)] = True
-                
-                # All r value
-                fig = plt.figure(figsize=(18, 14))
-                sns.heatmap(r,
-                            vmin=-1,
-                            vmax=1,
-                            cmap='RdBu_r',
-                            annot=True,
-                            fmt=".2f",
-                            annot_kws={"fontsize":6},
-                            mask=mask)
-                plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) ',
-                          fontsize=18, fontweight='bold')
-                pdf.savefig()
-                plt.close()
-                
-                # Significant r values
-                mask[(r.T >= .05)] = True
-                fig = plt.figure(figsize=(18, 14))
-                sns.heatmap(r,
-                            vmin=-1,
-                            vmax=1,
-                            cmap='RdBu_r',
-                            annot=True,
-                            fmt=".2f",
-                            annot_kws={"fontsize":6},
-                            mask=mask)
-                plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) significant (p<0.05)',
-                          fontsize=18, fontweight='bold')
-                pdf.savefig()
-                plt.close()
+                for i, txt in enumerate(labels_sub):
+                    ax.annotate(txt, (corr_data_sub.iloc[i,x], corr_data_sub.iloc[i,y]))
+                plt.title('study: {} | group: {} | condition: {}\n'.format(study, gp, cond)
+                          + regr_stats.to_string(col_space=10) + '\n' 
+                          + 'skipped: r = {:.2f}  p = {:.4f}  outliers: {}'.format(r, pval, ', '.join(labels_sub[outliers])))
+                plt.subplots_adjust(top=0.85)
+            
+            curr_func = partial(onclick, corr_data=corr_data, labels=labels, study=study, gp=gp, cond=cond)
+            fig.canvas.mpl_connect('button_press_event', curr_func)
+            sns.heatmap(r,
+                        vmin=-1,
+                        vmax=1,
+                        cmap='RdBu_r',
+                        annot=True,
+                        fmt=".2f",
+                        annot_kws={"fontsize":6},
+                        mask=mask)
+            plt.title(gp + ' - ' + cond + ' - ' + study + ' - correlation (Spearman) significant (p<0.05)',
+                      fontsize=18, fontweight='bold')
+            plt.subplots_adjust(left=0.2, right=0.9, top=0.9, bottom=0.2)
+                # pdf.savefig()
+                # plt.close()
