5f6f708d0d5a2476aac34f7df588476f30dff825
Ádám Nárai <narai.adam@gmail.com>
2020-04-06 05:12:33 +0200

    major fixes in compare_et_studies.py

--- bin/compare_et_studies.py
+++ bin/compare_et_studies.py
@@ -15,6 +15,9 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from na_py_tools.defaults import RESULTS_DIR, SETTINGS_DIR
 
+# Params
+save_pdf = False
+
 # Load params YAML
 with open(SETTINGS_DIR + '/params.yaml') as file:
     p = yaml.full_load(file)
@@ -23,7 +26,6 @@ with open(SETTINGS_DIR + '/params.yaml') as file:
 data = pd.read_pickle(
     os.path.join(RESULTS_DIR, 'df_data', 'all', 'et', 'et_features.pkl'))
 
-meas_list = p['params']['et']['meas_list']
 study_list = list(p['studies'].keys())
 
 # Create reports dir
@@ -31,7 +33,10 @@ results_dir = os.path.join(RESULTS_DIR, 'compare_studies', 'et_features')
 os.makedirs(results_dir, exist_ok=True)
 
 # %% Generate pdf figures
-with PdfPages(results_dir + '/ET_feature_compare.pdf') as pdf:      
+for meas_type in ['', '_all']:
+    meas_list = p['params']['et']['meas_list' + meas_type]
+    if save_pdf:
+        pdf = PdfPages(results_dir + '/ET_feature_compare' + meas_type + '.pdf')     
     for feature in meas_list:
         print('Running feature: {}'.format(feature))
         temp_data = dict()
@@ -53,29 +58,42 @@ with PdfPages(results_dir + '/ET_feature_compare.pdf') as pdf:
                     # meanprops={'marker':'.', 'markerfacecolor':'red', 'markeredgecolor':'red'}
         plt.ylabel(feature)
         
-        res = dict()
-        res[0] = pg.mwu(plot_data[plot_data.Study == study_list[0]].value,
-                             plot_data[plot_data.Study == study_list[1]].value)
-        res[1] = pg.mwu(plot_data[plot_data.Study == study_list[1]].value,
-                             plot_data[plot_data.Study == study_list[2]].value)
-        
-        # Stats
-        if res:
-            y_min = min(plot_data['value'])
-            y_max = max(plot_data['value'])
-            y = y_min-(y_max-y_min)*0.035
-            for i in range(0,2):
-                x1, x2 = i+0.1, i+0.9
-                p_str = '{:.5f}'.format(res[i].loc['MWU','p-val'])
-                if p_str:
-                    plt.plot([x1, x2], [y, y], lw=2, c=c)
-                    if res[i].loc['MWU','p-val'] >= .05:
-                        plt.text((x1+x2)*.5, y-(y_max-y_min)*0.015, p_str, ha='center', va='top', color=c, fontsize=8)
-                    else:
-                        plt.text((x1+x2)*.5, y-(y_max-y_min)*0.015, p_str, ha='center', va='top', color='r', fontsize=8)
+        y_min = min(plot_data['value'])
+        y_max = max(plot_data['value'])
+        for i in range(0,3):
+            if i == 2:
+                a = 0
+                b = 2
+                y = y_min-(y_max-y_min)*0.095
+            else:
+                a = i
+                b = i + 1
+                y = y_min-(y_max-y_min)*0.035
+            
+            # Stats (independent)
+            res_t = pg.ttest(x=plot_data[plot_data.Study == study_list[a]].value,
+                             y=plot_data[plot_data.Study == study_list[b]].value)
+            res_u = pg.mwu(x=plot_data[plot_data.Study == study_list[a]].value,
+                            y=plot_data[plot_data.Study == study_list[b]].value)
+            
+            x1, x2 = a+0.1, b-0.1
+            p_str = '{:.5f} / {:.5f}'.format(res_u.loc['MWU','p-val'], res_t.loc['T-test', 'p-val'])
+            plt.plot([x1, x2], [y, y], lw=2, c=c)
+            
+            # Text color
+            if (res_u.loc['MWU','p-val'] >= .05) and (res_t.loc['T-test', 'p-val'] >= 0.05):
+                color = c
+            else:
+                color = 'r'
+            plt.text((x1+x2)*.5, y-(y_max-y_min)*0.015, p_str, ha='center',
+                     va='top', color=color, fontsize=8)
+            plt.title('p-values: Welch T-test / Mann-Whitney U Test', fontsize = 8)
         
         plt.subplots_adjust(left=0.18, right=0.9, top=0.9, bottom=0.15)
-        pdf.savefig()
-        plt.close()
+        if save_pdf:
+            pdf.savefig()
+            plt.close()
+    if save_pdf:
+        pdf.close()
         
         
