a198ab41f5a5ef74ed609499aa374acb1e513298
Ádám Nárai <narai.adam@gmail.com>
2020-08-29 18:07:13 +0200

    adding data save for modeling

--- bin/et_feature_modeling.py
+++ bin/et_feature_modeling.py
@@ -27,6 +27,7 @@ import logging
 from datetime import datetime
 import warnings
 from tqdm import tqdm
+import pickle
 
 # Load params YAML
 p = load_params()
@@ -103,6 +104,9 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
                     cv_in_perm=False, binary_vars=[]):
     logger.info('Modeling {} in study: {}, group: {}, cond: {}'.format(y_name, study, gp, cond))
     
+    # Dict of all modeling data
+    model_data = dict()
+    
     # Get data
     X = df[X_names]
     y = df[y_name]
@@ -123,6 +127,9 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
     ols_model = LinearRegression().fit(X_st, y)
     ols_params = get_model_params(ols_model, X_st, y, X_names, fold_num, seed)
     logger.info('DONE.')
+    model_data['ols']['model'] = ols_model
+    model_data['ols']['params'] = ols_params
+    model_data['ols']['beta'] = ols_params['coef']
     
     # ENET hyperparameter (alpha, L1_ratio) selection and fit in one step
     logger.info('ENET hyperparameter selection using {}-fold cross-validation...'.format(fold_num))
@@ -130,13 +137,18 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
                               n_alphas=enet_alpha_N, max_iter=enet_max_iter, n_jobs=6)
     enet_params = get_model_params(enet_model.fit(X_st, y), X_st, y, X_names, fold_num, seed)
     logger.info('Best hyperparameters: alpha = {:.4f}  L1_ratio = {:.2f}'.format(enet_model.alpha_, enet_model.l1_ratio_))
+    model_data['enet']['model'] = enet_model
+    model_data['enet']['params'] = enet_params
+    model_data['enet']['beta'] = enet_params['coef']
     
     # ENET permuted p values
     logger.info('ENET p calculation using {} permutations...'.format(p_perm_num))
     enet_model_simple = ElasticNet(alpha=enet_model.alpha_, l1_ratio=enet_model.l1_ratio_, max_iter=enet_max_iter)
+    model_data['enet']['simple_model'] = enet_model_simple
     
     # Permutations
     np.random.seed(seed)
+    model_data['enet']['seed'] = seed
     perm_betas = np.empty([X_st.shape[1], p_perm_num-1])
     perm_betas[:] = np.nan
     for i in range(p_perm_num-1):
@@ -150,11 +162,14 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
             logger.info('Perm {:4} hyperparameters: alpha = {:.4f}  L1_ratio = {:.2f}'
                         .format(i+1, enet_model.alpha_, enet_model.l1_ratio_))
         perm_betas[:,i] = res.coef_
+
+    model_data['enet']['perm_betas'] = perm_betas
     
     # Get p values
     ge = np.sum(perm_betas <= enet_params['raw_coef'][:, None], axis=1)
     le = np.sum(perm_betas >= enet_params['raw_coef'][:, None], axis=1)
-    enet_p = np.minimum((2*np.minimum(ge,le)+1)/p_perm_num, np.ones(ge.shape))
+    enet_p = np.minimum((2*np.minimum(ge,le)+1)/p_perm_num, np.ones(ge.shape))    
+    model_data['enet']['pval'] = enet_p
     logger.info('DONE.')
     
     
@@ -163,13 +178,18 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
     ridge_model = RidgeCV(cv=fold_num, alphas=alpha_list)
     ridge_params = get_model_params(ridge_model.fit(X_st, y), X_st, y, X_names, fold_num, seed)
     logger.info('Best hyperparameters: alpha = {:.4f}'.format(ridge_model.alpha_))
+    model_data['ridge']['model'] = ridge_model
+    model_data['ridge']['params'] = ridge_params
+    model_data['ridge']['beta'] = ridge_params['coef']
     
     # Ridge permuted p values
     logger.info('Ridge p calculation using {} permutations...'.format(p_perm_num))
     ridge_model_simple = Ridge(alpha=ridge_model.alpha_)
+    model_data['ridge']['simple_model'] = ridge_model_simple
     
     # Permutations
     np.random.seed(seed)
+    model_data['ridge']['seed'] = seed
     perm_betas = np.empty([X_st.shape[1], p_perm_num-1])
     perm_betas[:] = np.nan
     for i in tqdm(range(p_perm_num-1)):
@@ -180,16 +200,18 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
             res = ridge_model.fit(X_st, y_perm)
             logger.info('Perm {:4} hyperparameters: alpha = {:.4f}'.format(i+1, ridge_model.alpha_))
         perm_betas[:,i] = res.coef_
+        
+    model_data['ridge']['perm_betas'] = perm_betas
     
     # Get p values
     ge = np.sum(perm_betas <= ridge_params['raw_coef'][:, None], axis=1)
     le = np.sum(perm_betas >= ridge_params['raw_coef'][:, None], axis=1)
-    ridge_p = (2*np.minimum(ge,le)+1)/p_perm_num
+    ridge_p = (2*np.minimum(ge,le)+1)/p_perm_num    
+    model_data['ridge']['pval'] = ridge_p
     logger.info('DONE.')
     
     # VIF
-    vif = pd.DataFrame(index=X_st.columns)
-    vif['VIF'] = [variance_inflation_factor(X_st.values, i) for i in range(X_st.shape[1])]
+    model_data['vif'] = [variance_inflation_factor(X_st.values, i) for i in range(X_st.shape[1])]
     
     # Get coeff df
     betas = pd.DataFrame(index = X_names)
@@ -236,6 +258,8 @@ def run_regressions(df, X_names, y_name, fold_num, seed, l1_ratio_list, alpha_li
     sm.OLS(y, sm.add_constant(X_st))
     res = sm.OLS(y, sm.add_constant(X_st)).fit()
     plt.text(0.1, 0, res.summary(), fontdict={'family':'monospace', 'fontsize':8})
+    model_data['ols']['stats'] = res
+    return model_data
     
 
 for et_features, fold_num in product(et_feature_list, fold_num_list):
@@ -259,9 +283,11 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                 pdf, out_path = create_pdf(study, gp, cond, et_features)
 
             # Run ENET and Ridge regressions
-            run_regressions(df, X_names, y_name, fold_num, seed, 
+            model_data = run_regressions(df, X_names, y_name, fold_num, seed, 
                             l1_ratio_list, alpha_list, study, gp, cond, cv_in_perm=cv_in_perm)
             
+            with open(out_path + '.pkl', 'wb') as file:
+                pickle.dump(model_data, file)
             if save_pdf:
                 pdf.savefig()
                 plt.savefig(out_path + '.png')
@@ -279,9 +305,11 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                 pdf, out_path = create_pdf(study, gp, cond, et_features)
             
             # Run ENET and Ridge regression
-            run_regressions(df, ['spacing_size'] + X_names, y_name, fold_num, seed, 
+            model_data = run_regressions(df, ['spacing_size'] + X_names, y_name, fold_num, seed, 
                             l1_ratio_list, alpha_list, study, gp, cond, cv_in_perm=cv_in_perm)
             
+            with open(out_path + '.pkl', 'wb') as file:
+                pickle.dump(model_data, file)
             if save_pdf:
                 pdf.savefig()
                 plt.savefig(out_path + '.png')
@@ -305,7 +333,7 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                         pdf, out_path = create_pdf(study, gp, cond, et_features + '/' + model_type + binary_vars_str)
                     if model_type == 'standard':
                         # Run standard regression
-                        run_regressions(df, ['group'] + X_names, y_name, fold_num, seed, 
+                        model_data = run_regressions(df, ['group'] + X_names, y_name, fold_num, seed, 
                                         l1_ratio_list, alpha_list, model_type + ': ' + study, gp, cond, 
                                         cv_in_perm=cv_in_perm, binary_vars=binary_vars)
                     elif model_type == 'interactions':
@@ -317,10 +345,12 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                             df[inter_name] = df['group'] * df[v]
                             
                         # Run regression with interactions
-                        run_regressions(df, ['group'] + X_names + inter_X_names, y_name, fold_num, seed, 
+                        model_data = run_regressions(df, ['group'] + X_names + inter_X_names, y_name, fold_num, seed, 
                                         l1_ratio_list, alpha_list, '(' + model_type + ') ' + study, gp, cond, 
                                         cv_in_perm=cv_in_perm, binary_vars=binary_vars)
                     
+                    with open(out_path + '.pkl', 'wb') as file:
+                        pickle.dump(model_data, file)
                     if save_pdf:
                         pdf.savefig()
                         plt.savefig(out_path + '.png')
@@ -346,7 +376,7 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                         pdf, out_path = create_pdf(study, gp, cond, et_features + '/' + model_type + binary_vars_str)
                     if model_type == 'standard':
                         # Run standard regression
-                        run_regressions(df, ['group'] + ['spacing_size'] + X_names, y_name, fold_num, seed, 
+                        model_data = run_regressions(df, ['group'] + ['spacing_size'] + X_names, y_name, fold_num, seed, 
                                         l1_ratio_list, alpha_list, model_type + ': ' + study, gp, cond, 
                                         cv_in_perm=cv_in_perm, binary_vars=binary_vars)
                     elif model_type == 'interactions':
@@ -358,10 +388,12 @@ for et_features, fold_num in product(et_feature_list, fold_num_list):
                             df[inter_name] = df['group'] * df[v]
                             
                         # Run regression with interactions
-                        run_regressions(df, ['group'] + ['spacing_size'] + X_names + inter_X_names, y_name, fold_num, seed, 
+                        model_data = run_regressions(df, ['group'] + ['spacing_size'] + X_names + inter_X_names, y_name, fold_num, seed, 
                                         l1_ratio_list, alpha_list, '(' + model_type + ') ' + study, gp, cond, 
                                         cv_in_perm=cv_in_perm, binary_vars=binary_vars)
             
+                    with open(out_path + '.pkl', 'wb') as file:
+                        pickle.dump(model_data, file)
                     if save_pdf:
                         pdf.savefig()
                         plt.savefig(out_path + '.png')
