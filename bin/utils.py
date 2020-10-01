b1d9c5a1eeb13ed54fb22f5e63de990875cc9455
Ádám Nárai <narai.adam@gmail.com>
2020-10-01 20:17:33 +0200

    adding try for proofreading and sentence verification

--- bin/utils.py
+++ bin/utils.py
@@ -45,26 +45,32 @@ def import_et_behav_data(p, experiments=['et', '3dmh', 'wais', 'perf', 'vsp', 'w
         os.path.join(RESULTS_DIR, 'df_data', 'all', 'et', 'word_info.pkl'))
     meas_list['word_info'] = p['params']['et']['word_info_list']
     
-    # Proofreading
-    data_proof = pd.read_pickle(
-        os.path.join(RESULTS_DIR, 'df_data', 'dys_study', 'proofreading', 'proofreading.pkl'))
-    data_proof.columns = ['_'.join([c, 'proof']) if c in p['params']['proofreading'][et_feature_list] else c for c in data_proof.columns]
-    meas_list['proofreading'] = ['_'.join([s, 'proof']) for s in p['params']['proofreading'][et_feature_list]]
-    
-    # Proofreading
-    data_verif = pd.read_pickle(
-        os.path.join(RESULTS_DIR, 'df_data', 'dys_study', 'sentence_verification', 'sentence_verification.pkl'))
-    data_verif.columns = ['_'.join([c, 'verif']) if c in p['params']['sentence_verification'][et_feature_list] else c for c in data_verif.columns]
-    meas_list['sentence_verification'] = ['_'.join([s, 'verif']) for s in p['params']['sentence_verification'][et_feature_list]]
-
     # Merge all data
     data = data_et.merge(data_3dmh, how='outer', on=['study', 'group', 'subj_id'])
     data = data.merge(data_wais, how='outer', on=['study', 'group', 'subj_id'])
     data = data.merge(data_vsp, how='outer', on=['group', 'condition', 'spacing_size', 'subj_id'])
     data = data.merge(data_perf, how='outer', on=['study', 'group', 'condition', 'spacing_size', 'subj_id'])
     data = data.merge(data_word_info, how='outer', on=['study', 'group', 'condition', 'subj_id'])
-    data = data.merge(data_proof, how='outer', on=['study', 'group', 'subj_id'])
-    data = data.merge(data_verif, how='outer', on=['study', 'group', 'subj_id'])
+    
+    # Proofreading
+    try:
+        data_proof = pd.read_pickle(
+            os.path.join(RESULTS_DIR, 'df_data', 'dys_study', 'proofreading', 'proofreading.pkl'))
+        data_proof.columns = ['_'.join([c, 'proof']) if c in p['params']['proofreading'][et_feature_list] else c for c in data_proof.columns]
+        meas_list['proofreading'] = ['_'.join([s, 'proof']) for s in p['params']['proofreading'][et_feature_list]]
+        data = data.merge(data_proof, how='outer', on=['study', 'group', 'subj_id'])
+    except:
+        print('Could not load proofreading data.')
+    
+    # Sentence verification
+    try:
+        data_verif = pd.read_pickle(
+            os.path.join(RESULTS_DIR, 'df_data', 'dys_study', 'sentence_verification', 'sentence_verification.pkl'))
+        data_verif.columns = ['_'.join([c, 'verif']) if c in p['params']['sentence_verification'][et_feature_list] else c for c in data_verif.columns]
+        meas_list['sentence_verification'] = ['_'.join([s, 'verif']) for s in p['params']['sentence_verification'][et_feature_list]]
+        data = data.merge(data_verif, how='outer', on=['study', 'group', 'subj_id'])
+    except:
+        print('Could not load sentence verification data.')
 
     # Merge all measure varname
     plot_meas_list = sum([meas_list[exp] for exp in experiments], [])
